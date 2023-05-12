94050fd1c9d4d1ae359c2483513ab88204929790
github_username <you@domain.com>
2023-05-12 18:26:18 +0600

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import AddCoffe from './components/AddCoffe.jsx';
+import UpdateCoffee from './components/UpdateCoffee.jsx';
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App></App>,
+  },
+  {
+    path: "/addcoffe",
+    element: <AddCoffe></AddCoffe>,
+  },
+  {
+    path: "/updatecoffe",
+    element: <UpdateCoffee></UpdateCoffee>,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+     <RouterProvider router={router} />
+  </React.StrictMode>,
+)
